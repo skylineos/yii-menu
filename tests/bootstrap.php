5889c306aeb213b4b8ae70bf2dd527e7c536d8ae
Damien Kemens <dkemens@skylinenet.net>
2021-08-17 09:06:45 -0400

    added tests folder (no tests yet)

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+\Codeception\Util\Autoload::addNamespace('', 'vendor/yiisoft/yii2');
+\Codeception\Util\Autoload::addNamespace('', 'vendor/yiisoft/yii2/base');
+\Codeception\Util\Autoload::addNamespace('', 'vendor/yiisoft/yii2/db');
+\Codeception\Util\Autoload::addNamespace('', 'vendor/skyline/yii-menu/src/models/');
+\Codeception\Util\Autoload::addNamespace('', 'tests/fixtures');
+\Codeception\Util\Autoload::addNamespace('', 'models');
+\Codeception\Util\Autoload::addNamespace('', 'controllers');
